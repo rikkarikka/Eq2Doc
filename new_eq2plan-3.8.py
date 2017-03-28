f9a8af5e372881aa62a634a67371f79520c9ab93
rikka <kedzior@uw.edu>
2017-03-27 19:22:03 -0700

    outputs

--- new_eq2plan-3.8.py
+++ new_eq2plan-3.8.py
@@ -177,12 +177,12 @@ def val(net,src,tgt,eq_v,out_v):
     else:
       outputs,_ = net(Variable(torch.LongTensor(s).view(1,-1)),Variable(dps),hc,hc_2)
     tstr = ""
-    #tstr += ' '.join([ev[x] for x in s]) + '\n'
+    # measure accuracy up to 5th pred?
     acc += sum([int(outputs[i].data[0]==tgt[c][i]) for i in range(50)])/50
     for j in range(5):
       if out_v[int(outputs[(j*10)].data[0])] == "<NULL>":
         break
-      tstr = ' '.join([out_v[int(outputs[(j*10)+k].data[0])] for k in range(9)])
+      tstr += ' '.join([out_v[int(outputs[(j*10)+k].data[0])] for k in range(9)])
       if out_v[int(outputs[(j*10)+9].data[0])] == "NEW_S":
         tstr += " EOS "
       else:
@@ -194,7 +194,7 @@ def val(net,src,tgt,eq_v,out_v):
   return ostr,acc
 
 
-def train():
+def train(out_dir):
   with open("pickles/eq2planData.pickle",'rb') as f:
     fns, eqs, dps, _ = pickle.load(f)
 
@@ -226,7 +226,6 @@ def train():
   for epoch in range(EPOCHS):
     do_epoch(net,optimizer,criterion,train_src,train_tgt,batches,epoch)
 
-    ostr,acc = val(net,dev_src,dev_tgt,eq_v,out_v)
     if epoch % 10 == 9:
       print("Writing %d checkpoint" % epoch)
       checkpoint = {
@@ -235,16 +234,19 @@ def train():
         'vecs': (train_src,train_tgt,dev_src,dev_tgt),
         'epoch':epoch
       }
-      torch.save(checkpoint, "saved_models/"+str(epoch)+'checkpoint.mod')
+      torch.save(checkpoint, out_dir+"/"+str(epoch)+'checkpoint.mod')
       ostr,acc = val(net,dev_src,dev_tgt,eq_v,out_v)
       print("Valid Accuracy: %f" %acc)
-      with open("val-"+str(acc)+"-e"+str(epoch)+".out",'w') as f:
+      with open(out_dir+"/val-"+str(acc)+"-e"+str(epoch)+".out",'w') as f:
         f.write(ostr)
 
 if __name__=="__main__":
 
   if sys.argv[1] == "train":
-    train()
+    try:
+      train(sys.argv[2])
+    except:
+      usage()
 
   elif sys.argv[1] == "val":
     with open(sys.argv[2],'rb') as f:
